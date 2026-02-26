baba2c1942179609bb17351346be93a4b78c5ecc
Grant Lemons <grantlemons@aol.com>
2026-02-26 09:52:18 -0700

    remove computer and memory from project

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -21,7 +21,7 @@ class TestBase(unittest.TestCase):
         if not os.path.isfile(f'/autograder/grader/tests/{name}_test.v'):
             raise AssertionError(f'{name}_test.v not found!')
 
-        res = subprocess.call(['iverilog', '-o', f'/tmp/{name}_test.vvp', f'/autograder/grader/tests/{name}_test.v', '-l/autograder/grader/tests/dff.v', '-l/autograder/grader/tests/muxlib.v', '-l/autograder/grader/tests/nand.v'] + [f'-l{p}' for p in glob.glob('/autograder/source/*.v')])
+        res = subprocess.call(['iverilog', '-o', f'/tmp/{name}_test.vvp', f'/autograder/grader/tests/{name}_test.v', '-l/autograder/grader/tests/dff.v', '-l/autograder/grader/tests/muxlib.v', '-l/autograder/grader/tests/nand.v', '-l/autograder/grader/tests/memory.v', '-l/autograder/grader/tests/computer.v'] + [f'-l{p}' for p in glob.glob('/autograder/source/*.v')])
         if res != 0:
             raise AssertionError('Unable to build verilog + test script to vvp!')
 
